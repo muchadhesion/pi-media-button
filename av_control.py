97316bcabf6de140db2942d39351d3db157e470d
muchadhesion <github@muchadhesion.info>
2016-10-09 19:46:55 +0100

    Add av_control.get_playstate() and tests

--- av_control.py
+++ av_control.py
@@ -1,4 +1,6 @@
 import requests
+import xml.etree.ElementTree as ET
+
 
 def join_dicts(x, y):
     '''Given two dicts, merge them into a new dict as a shallow copy.'''
@@ -76,4 +78,46 @@ def radio_stop(url_base, control_url = 'Ds/Radio/control'):
 			)
 	print r.status_code
 	print r.text
+
+def get_playstate(url_base, control_url = 'Ds/Radio/control'):
+	r = requests.post('{0}/{1}'.format( url_base, control_url ) ,
+		headers = join_dicts( default_headers(), {
+			'soapaction': '"urn:av-openhome-org:service:Radio:1#TransportState"',
+		} ),
+		data = '<?xml version="1.0" encoding="utf-8"?>'
+			'<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"'
+			's:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'
+				'<s:Body>'
+				'<u:TransportState xmlns:u="urn:av-openhome-org:service:Radio:1"></u:TransportState>'
+			'</s:Body>'
+			'</s:Envelope>'
+			)
+	print r.status_code
+	print r.text
+	tree = ET.fromstring(r.text)
+	r = tree.find('.//Value').text
+	print r
+	return r
+
+
+def test_get_playstate(url_base):
+	import time
+	radio_play(url_base)
+	tries = 0
+	r = 'unknown'
+	while r != 'Playing':
+		if tries > 3:
+			raise Exception("test failed")
+		tries += 1
+		r = get_playstate(url_base)
+		time.sleep(1)
+
+	radio_stop(url_base)
+	time.sleep(1)
+	r = get_playstate(url_base)
+	if r != 'Stopped':
+		raise Exception("test failed")
+
+def tests(url_base = 'http://192.168.1.84:55178'):
+	test_get_playstate(url_base)
 
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 
 python -c 'from media_button import *; test_volume_up()'
 python -c 'from media_button import *; test_radio_play()'
+python -c 'from av_control import *; tests()'
 
 if [ "$USER" = "pi" ]; then
 	python -c 'from media_button import *; test_find_button_device("Satechi Media Button")'
