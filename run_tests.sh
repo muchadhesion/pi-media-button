b53a5c8b9bd88a2d4329d6fae6408ae722e967e5
muchadhesion <github@muchadhesion.info>
2016-10-09 17:56:28 +0100

    Added missing run_test.sh entry for test_radio_play()

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 
 python -c 'from media_button import *; test_volume_up()'
+python -c 'from media_button import *; test_radio_play()'
 
 if [ "$USER" = "pi" ]; then
 	python -c 'from media_button import *; test_find_button_device("Satechi Media Button")'
